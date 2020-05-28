957b29c7ce40c66ace86330e84bce88aff286781
tanu <thakaretanushree@gmail.com>
2020-05-28 18:41:42 +0530

    fblogin

---
+++ app/src/main/java/com/tanu/facebooklogin/registeractivity.java
@@ -0,0 +1,14 @@
+package com.tanu.facebooklogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registeractivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeractivity);
+    }
+}
